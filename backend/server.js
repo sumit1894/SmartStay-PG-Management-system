efd04f99c45c7a880dd2b2d7b77a67566ebfd0f9
sumit1894 <sumitprasad1971@gmail.com>
2026-03-19 19:40:00 +0530

    smartStay-host

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,23 @@ import { getDashboardStats } from "./Dashboard/dashboardController/getDashboard.
 const app = express();
 import cors from "cors"
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://spstacksmartpg.netlify.app"
+];
+
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://your-netlify-app.netlify.app"
-  ],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
-}))
+}));
+
+
 app.use(express.json());
 
 app.use("/api/auth", userAuthRoute);
